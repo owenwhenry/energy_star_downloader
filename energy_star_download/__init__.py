42c7072d1eaf7ab6f66bcdbeeea4453c0cb1e3a7
Owen <henryowenw@gmail.com>
2025-05-10 14:50:19 -0400

    Fixed connection string and requirements

--- energy_star_download/__init__.py
+++ energy_star_download/__init__.py
@@ -33,7 +33,7 @@ def main(mytimer: func.TimerRequest) -> None:
 
             logging.info("Uploading to Azure Blob Storage...")
             
-            blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;")
+            blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
             container_client = blob_service_client.get_container_client(container_name)
 
